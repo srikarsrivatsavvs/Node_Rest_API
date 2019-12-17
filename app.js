0080b39b90a64076c65172eef8d40964a7116fe9
Srikar Srivatsav <srikarsrivatsav@Srikars-MacBook-Air.local>
2019-12-17 21:12:00 +0530

    Adding app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+var path = require("path");
+var fs = require("fs");
+const cloudinary = require("cloudinary");
+global.__basedir = __dirname;
+
+app.use(bodyParser.json({ limit: "10mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+require("./routes/customer.route")(app);
+require("./routes/caterer.route")(app);
+require("./routes/menu.route")(app);
+require("./routes/cart.route")(app);
+require("./routes/order.route")(app);
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+const mongoose = require("mongoose");
+// Your Mongo Atlas Cluster
+// Create a Project on Mongo Atlas and Create a Cluster and than configure it
+let dev_db_url =
+  // "mongodb+srv://<username>:<password>@cluster0-zevrx.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb://35.200.243.49:27017/catersmart";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected…"))
+  .catch(err => console.log(err));
+
+app.listen(process.env.PORT || 4000);
+
+// Please create your account on cloudinary and find following keys from Dashboard.
+// Make Sure you verified your account via Email after creating to work it properly.
+cloudinary.config({
+  cloud_name: "premshamsundar",
+  api_key: "425991328347625",
+  api_secret: "4Z4-DI9OAScG2SP8dZzBDtzCAbQ"
+});
