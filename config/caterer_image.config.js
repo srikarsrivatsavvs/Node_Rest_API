f8c9024e0ef667bc4a1d1b41d21eff20ee6712ce
Windows10 <prem.shyam01@gmail.com>
2019-12-19 23:08:10 +0530

    wrote comments for caterer image config.js and added multer fileFilter configuration for allowing only png, jpg or jpeg

--- config/caterer_image.config.js
+++ config/caterer_image.config.js
@@ -1,14 +1,37 @@
-const multer = require('multer');
-var path = require('path');
-
-    const storage = multer.diskStorage({
-        destination: function(req, file, cb){
-            cb(null, __basedir + '/images/');
-        },
-        filename: function(req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-        }
-    });
-    const upload = multer({storage: storage});
-
-    module.exports = upload;
+//middleware for handling multipart/form-data
+const multer = require("multer");
+//built-in module To handle file paths
+var path = require("path");
+//file storage configuration
+const storage = multer.diskStorage({
+  //setting destination to save the file on the server
+  destination: (req, file, cb) => {
+    //directory to the file
+    cb(null, __basedir + "/images/");
+  },
+  //setting filename for the uploaded file
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      //fieldname is image + - + epochtime + uploaded file extention
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+//file filter configuration
+const fileFilter = (req, file, cb) => {
+  //accept files only with png, jpg or jpge
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+//multer configuration
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+//export it as a module
+module.exports = upload;
